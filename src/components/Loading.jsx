dca1607a5ff6f413db42fc27614a4fbbc29f0c50
ElionorVendrell <elivendrell@gmail.com>
2023-04-19 19:12:01 +0200

    Loading style

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "../styles/loading.css";
 
 export const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className='wrapper'>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='shadow'></div>
+      <div className='shadow'></div>
+      <div className='shadow'></div>
+      <span>Loading</span>
+    </div>
+  );
 };

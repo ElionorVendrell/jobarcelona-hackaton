6109023081ac1f86f7dae8660d6a401f5867464d
ElionorVendrell <elivendrell@gmail.com>
2023-04-19 21:05:24 +0200

    show recipe details & localStorage

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -1,13 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { RecipesContext } from "../context/RecipesContext";
 
 export const RecipeDetailPage = () => {
   const { id } = useParams();
+  const [recipeDetail, setRecipeDetail] = useState(null);
 
   const infoRecipes = useContext(RecipesContext);
   const recipes = infoRecipes.recipes;
 
-  return <div>receta {id}</div>;
+  const cleanInstructions = (instructions) => {
+    return instructions.replace(/<ol>|<\/ol>|<li>|<\/li>/g, "");
+  };
+
+  useEffect(() => {
+
+    const recipesFromStorage = localStorage.getItem("recipes");
+    if (recipesFromStorage) {
+
+      infoRecipes.setRecipes(JSON.parse(recipesFromStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+
+    const recipe = recipes.find((recipe) => recipe.id === parseInt(id));
+    if (recipe) {
+
+      localStorage.setItem("recipeDetail", JSON.stringify(recipe));
+      setRecipeDetail(recipe);
+    } else {
+
+      const recipeDetailFromStorage = localStorage.getItem("recipeDetail");
+      if (recipeDetailFromStorage) {
+        setRecipeDetail(JSON.parse(recipeDetailFromStorage));
+      }
+    }
+  }, [id, recipes]);
+
+  if (!recipeDetail) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{recipeDetail.title}</h1>
+      <p>{cleanInstructions(recipeDetail.instructions)}</p>
+    </div>
+  );
 };

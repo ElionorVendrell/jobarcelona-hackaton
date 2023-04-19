410489058afa8201237ed0e05faced5dc51041fb
ElionorVendrell <elivendrell@gmail.com>
2023-04-19 18:07:42 +0200

    routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,2 +1,15 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { RecipeListPage } from "../pages/RecipeListPage";
+import { RecipeDetailPage } from "../pages/RecipeDetailPage";
+
+const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<RecipeListPage />} />
+      <Route path='/RecipeDetail/:recipeName' element={<RecipeDetailPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+export default Router;

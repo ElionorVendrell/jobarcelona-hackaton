7bafb1367114470a0adce209fba99373c1606834
ElionorVendrell <elivendrell@gmail.com>
2023-04-19 22:42:08 +0200

    show info recipe details

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import { MdRestaurant, MdAccessTime } from "react-icons/md";
+
+import { Loading } from "../components/Loading";
 import { RecipesContext } from "../context/RecipesContext";
 
 export const RecipeDetailPage = () => {
@@ -15,23 +18,18 @@ export const RecipeDetailPage = () => {
   };
 
   useEffect(() => {
-
     const recipesFromStorage = localStorage.getItem("recipes");
     if (recipesFromStorage) {
-
       infoRecipes.setRecipes(JSON.parse(recipesFromStorage));
     }
   }, []);
 
   useEffect(() => {
-
     const recipe = recipes.find((recipe) => recipe.id === parseInt(id));
     if (recipe) {
-
       localStorage.setItem("recipeDetail", JSON.stringify(recipe));
       setRecipeDetail(recipe);
     } else {
-
       const recipeDetailFromStorage = localStorage.getItem("recipeDetail");
       if (recipeDetailFromStorage) {
         setRecipeDetail(JSON.parse(recipeDetailFromStorage));
@@ -40,12 +38,51 @@ export const RecipeDetailPage = () => {
   }, [id, recipes]);
 
   if (!recipeDetail) {
-    return <div>Cargando...</div>;
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
   }
 
+  const urlImage = recipeDetail.image;
+  const image = urlImage;
+  const ingredients = recipeDetail.extendedIngredients;
   return (
     <div>
+      <div className='card lg:card-side bg-base-100 shadow-xl'>
+        <figure>
+          <img src={image} alt='imagen del plato' />
+        </figure>
+        <div className='card-body'>
+          <h1 className='card-title'>{recipeDetail.title}</h1>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className='card-actions justify-end'>
+            <button className='btn btn-primary'>Listen</button>
+          </div>
+        </div>
+      </div>
+
       <h1>{recipeDetail.title}</h1>
+      <img src={image} />
+
+      {recipeDetail.glutenFree ? <p>gluten free</p> : null}
+      {recipeDetail.vegetarian ? <p>vegetarian</p> : null}
+      {recipeDetail.vegan ? <p>vegan</p> : null}
+
+      <MdRestaurant />
+      <p>{recipeDetail.servings}</p>
+
+      <MdAccessTime />
+      <p>{recipeDetail.readyInMinutes} min</p>
+
+      <p>{recipeDetail.pricePerServing} $</p>
+
+      <p>
+        {ingredients.map((recipe) => (
+          <li>{recipe.original}</li>
+        ))}
+      </p>
       <p>{cleanInstructions(recipeDetail.instructions)}</p>
     </div>
   );
